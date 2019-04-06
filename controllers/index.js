b690515dd74bc9fec643763ab9541b3caf0267b8
Karn Rahal <karn.rahal@alumni.ubc.ca>
2019-04-05 19:25:15 -0700

    set up nodemailer to send contact form data to email

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,17 @@
+const nodemailer = require('nodemailer');
+const AWS = require('aws-sdk');
+
+// configure AWS SDK
+AWS.config.loadFromPath('config.json');
+
+// create Nodemailer SES transporter
+const transporter = nodemailer.createTransport({
+  SES: new AWS.SES({
+    apiVersion: '2010-12-01',
+  }),
+});
+
+
 exports.index = (req, res) => {
   res.render('index');
 };
@@ -11,5 +25,19 @@ exports.contact = (req, res) => {
 };
 
 exports.sendMessage = (req, res) => {
-  res.render('');
+  const {
+    name,
+    number,
+    email,
+    message,
+  } = req.body;
+
+  transporter.sendMail({
+    from: 'rahalkarn@gmail.com',
+    to: 'karnrahal@live.com',
+    subject: `Contact form message - ${name}`,
+    text: `Name: ${name}\nEmail: ${email}\nPhone Number: ${number}\n\n${message}`,
+  }, () => {
+    res.render('contact');
+  });
 };
